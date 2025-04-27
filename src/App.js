f73d452e6dfdf7f4c8392c76f7ae24a46b9055e1
morenotgroup <gc@agenciataj.com>
2025-04-26 23:17:47 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{
+      fontFamily: "Montserrat, sans-serif",
+      textAlign: "center",
+      paddingTop: "50px"
+    }}>
+      <h1>Bem-vindo ao T Pulse</h1>
+      <p>O app oficial do T Group para todos os colaboradores!</p>
+    </div>
+  );
+}
+
+export default App;
