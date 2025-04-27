1c31eec96efddd3da283466006b1636245878fb0
morenotgroup <gc@agenciataj.com>
2025-04-27 01:50:06 -0300

    Update Checkin.js

--- src/pages/Checkin.js
+++ src/pages/Checkin.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-function Home() {
+function Checkin() {
   return (
-    <div style={{ fontFamily: "Montserrat, sans-serif", textAlign: "center", marginTop: "50px" }}>
-      <h2>Checkin Almoço</h2>
-      <p>Bem-vindo ao aplicativo de intranet do T Group!</p>
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>Check-in do Almoço</h2>
+      <p style={{ marginBottom: "20px" }}>Clique abaixo para fazer seu check-in:</p>
+      <a href="https://script.google.com/macros/s/AKfycbw2nUG1r-_051Lbb6RBxuUHyqLRsBYq99hNB2panv8bbGryplPMzZo6yN8HCLg13tKR/exec" target="_blank" rel="noopener noreferrer" style={botao}>
+        Fazer Check-in
+      </a>
     </div>
   );
 }
 
-export default Home;
+const botao = {
+  display: "inline-block",
+  padding: "10px 20px",
+  backgroundColor: "#ff7a00",
+  color: "#fff",
+  borderRadius: "8px",
+  textDecoration: "none",
+  fontWeight: "600",
+  marginTop: "10px"
+};
+
+export default Checkin;
