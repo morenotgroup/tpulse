5cf70cbbea81368b799cfb090a0155a0299345c2
morenotgroup <gc@agenciataj.com>
2025-04-26 23:48:14 -0300

    Create Aniversariantes.js

---
+++ src/pages/Aniversariantes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ fontFamily: "Montserrat, sans-serif", textAlign: "center", marginTop: "50px" }}>
+      <h2>Aniversariantes</h2>
+      <p>Bem-vindo ao aplicativo de intranet do T Group!</p>
+    </div>
+  );
+}
+
+export default Home;
